8d5eee34554f799986176f3d253f9633f501365a
Yuji Sasaki <jyu0414@gmail.com>
2022-12-15 13:00:26 +0900

    fix print

--- gp4cg/FormulaTree/Node.swift
+++ gp4cg/FormulaTree/Node.swift
@@ -69,7 +69,7 @@ struct Node {
 extension Node: CustomStringConvertible {
     var description: String {
         if op.arity == 2 {
-            if let p1 = params[0] as? Int, let p2 = params[1] as? Int {
+            if let p1 = Int("\(params[0])"), let p2 = Int("\(params[1])") {
                 return "\(op.function([p1, p2]))"
             } else {
                 return "(\(params[0]) \(op.symbol) \(params[1]))"
@@ -85,12 +85,12 @@ extension Node: CustomStringConvertible {
     
     func toFunctionStyleString() -> String {
         if op.arity == 2 {
-            if let p1 = params[0] as? Int, let p2 = params[1] as? Int {
+            if let p1 = Int("\(params[0])"), let p2 = Int("\(params[1])") {
                 return "\(op.function([p1, p2]))"
             } else if op.name == "xor" {
                 return "BitXor[\(params[0].toFunctionStyleString()),\(params[1].toFunctionStyleString())]"
             } else {
-                return "(\(params[0]) \(op.symbol) \(params[1]))"
+                return "(\(params[0].toFunctionStyleString()) \(op.symbol) \(params[1].toFunctionStyleString()))"
             }
         } else {
             if let p = params[0] as? Int {
