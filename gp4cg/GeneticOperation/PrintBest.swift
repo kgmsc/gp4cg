96c2aabd7606136c1abb023192c76569204518c9
Tanesan <keito.12.10@icloud.com>
2023-01-17 09:41:09 +0900

    エラー修正

--- gp4cg/GeneticOperation/PrintBest.swift
+++ gp4cg/GeneticOperation/PrintBest.swift
@@ -6,13 +6,15 @@
 //
 
 import Foundation
-
 /// This code is a genetic operation that prints the best individual in a population.
 /// The best individual is determined by the sort function passed in to the constructor.
-func exec(_ population: [Individual], generation: Int) -> [Individual] {
-    if let min = population.min(by: sortFunction) {
-        print("\tBest: \(min)")
+struct PrintBest: GeneticOperation {
+    let sortFunction: (Individual, Individual) -> Bool
+    var description: String = ""
+    func exec(_ population: [Individual], generation: Int) -> [Individual] {
+        if let min = population.min(by: sortFunction) {
+            print("\tBest: \(min)")
+        }
+        return population
     }
-    return population
 }
-
